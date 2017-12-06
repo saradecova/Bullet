cd412ff58773b0aef0c08791519023c807b5abdf
saradecova <sara.decova@gmail.com>
2017-12-06 17:46:13 +0000

    added constraints

--- ShortSim/short_chain.cpp
+++ ShortSim/short_chain.cpp
@@ -5,13 +5,36 @@
 #include <memory>
 #include <vector>
 
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_real_distribution.hpp>
+#include <boost/random/normal_distribution.hpp>
 
 const double kChromRadiusMono = 1e-8;
 const double kChromLength = 1.25e-8;
-const double kChromMass = 13e6*1.7e-27/5;
+const double kMass= 13e6*1.7e-27/5;
 const double kCylLength = 15e-8;
 
+// langevin force constants
+const double kViscosity = 6.6e-3;
+const double kB = 1.38e-23;
+const double kTemp = 310.;
+const double kFriction=6.0*M_PI*kViscosity*20e-9;
+const double kDT = kMass/kFriction; //delta time (time step)
+const int kNoDT = 5000;
+const int kDuration = kNoDT*kDT;
+const double kSigma = sqrt(2.0*kFriction*kB*kTemp/kDT);
+
 const int kNoSegments = 18;
+
+// gaussian distribution for langevin force
+boost::random::mt19937 rng;
+
+boost::function<double()> randu =
+       boost::bind(boost::random::uniform_real_distribution<>(0, 1), rng);
+boost::function<double()> randn =
+       boost::bind(boost::random::normal_distribution<>(0, 1), rng);
 
 void loadSegmentData(std::vector<std::vector<double> >* segments,
 		     std::vector<std::vector<double> >* poles) {
@@ -39,6 +62,23 @@ void loadSegmentData(std::vector<std::vector<double> >* segments,
     }
   }
   else std::cout << "Unable to open file InitCoord25e-9_18seg.txt" << std::endl;
+}
+
+void setLangevinTr(btRigidBody* body, btVector3* rand_force, btVector3* friction) {
+  const btVector3& lin_velocity = body->getLinearVelocity();
+  // std::cout << "velocity: " << lin_velocity[0] << " " << lin_velocity[1] << " " << lin_velocity[2] << std::endl;
+  btVector3 force;
+  //force = - kFriction * lin_velocity + btVector3(randn() * kSigma, randn() * kSigma, randn() * kSigma);
+  force[0] = 2000. * kSigma * (0.5-randu()) - kFriction * lin_velocity[0]/50;
+  force[1] = 2000. * kSigma * (0.5-randu()) - kFriction * lin_velocity[1]/50;
+  force[2] = 2000. * kSigma * (0.5-randu()) - kFriction * lin_velocity[2]/50;
+  // std::cout << "FORCE: "
+  //           << force[0] << " "
+  //           << force[1] << " "
+  //           << force[2] << std::endl;
+  body->applyCentralForce(force);
+  *rand_force = force + kFriction*lin_velocity/50;
+  *friction = kFriction*lin_velocity/50;
 }
 
 int main () {
@@ -69,12 +109,13 @@ int main () {
     // Create Chromosome mono shape
     double len = kChromLength-0.9e-8;
     btCollisionShape* capsuleShape = new btCapsuleShapeZ(kChromRadiusMono,len);
-    btScalar mass(kChromMass);
+    btScalar mass(kMass);
     btVector3 inertia(1., 1., 1.);
     capsuleShape->calculateLocalInertia(mass, inertia);
     
     btTransform startTransform;
     startTransform.setIdentity();
+    btAlignedObjectArray<btRigidBody*> cylinders;
     for(int i=0;i<kNoSegments;++i) {
       startTransform.setOrigin(btVector3(btScalar(segments[i][1]),
 					 btScalar(segments[i][2]),
@@ -83,6 +124,7 @@ int main () {
       btRigidBody::btRigidBodyConstructionInfo body_ci(mass, motion_state, capsuleShape, inertia);
       btRigidBody* capsule_body = new btRigidBody(body_ci);
       capsule_body->setUserIndex2(i);
+      cylinders.push_back(capsule_body);
       dynamicsWorld->addRigidBody(capsule_body);
     }
 
@@ -102,8 +144,28 @@ int main () {
     dynamicsWorld->addRigidBody(pole_body);
 
     // Set up constraints:
+    // add N-1 spring constraints
+    for(int i=0;i<kNoSegments;++i) {
+      btRigidBody* b1 = cylinders[i];
+      btRigidBody* b2 = cylinders[i+1];
+      btPoint2PointConstraint* joint =
+	new btPoint2PointConstraint(*b1, *b2,
+				    btVector3(0,0,-1*kCylLength),
+				    btVector3(0,0,kCylLength));
+      dynamicsWorld->addConstraint(joint);
+    }
+    // add contraint between 4th cylinder and pole:
+    btRigidBody* b1 = cylinders[3];
     
-    
+    btPoint2PointConstraint* pole_chrom_joint =
+      new btPoint2PointConstraint(*b1, *pole_body,
+				  btVector3(0,-1*kChromRadiusMono,0),
+				  btVector3(0,kChromLength,0));
+    dynamicsWorld->addConstraint(pole_chrom_joint);
+    btPoint2PointConstraint* pole_joint =
+      new btPoint2PointConstraint(*pole_body, btVector3(0, -1*kChromLength,0));
+    dynamicsWorld->addConstraint(pole_joint);
+
     // Clean up behind ourselves like good little programmers
     delete dynamicsWorld;
     delete solver;
